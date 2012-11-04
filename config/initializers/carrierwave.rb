46b654723009c939e747e3ae3e175bf3b98cbcf3
Dan Koslow <dan.koslow@gmail.com>
2012-11-04 17:31:47 -0500

    Integrate carrierwave pt.2.

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,11 @@
+CarrierWave.configure do |config|
+
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => 'AKIAJACUC52KL5LEONTQ',
+    :aws_secret_access_key  => ENV['S3_SECRET_ACCESS_KEY']
+  }
+
+  config.fog_directory  = "dkoslow_blog_#{Rails.env}"
+  config.fog_public     = true
+end
